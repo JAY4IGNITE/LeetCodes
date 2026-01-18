0dd4b3a32c2aec3583c6f9d3e4bb8178b68b2c56
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-01-18 22:41:57 +0530

    Time: 3 ms (7.9%), Space: 9.5 MB (99%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>ans;
+        for(int i=0;i<numRows;i++){
+            vector<int>a(i+1);
+            ans.push_back(a);
+            for (int j = 0; j <= i; j++){
+                if (j == 0 || j == i) ans[i][j] = 1;
+                else ans[i][j] = ans[i-1][j] + ans[i-1][j-1];
+            }
+        }
+        return ans;
+    }
+};
