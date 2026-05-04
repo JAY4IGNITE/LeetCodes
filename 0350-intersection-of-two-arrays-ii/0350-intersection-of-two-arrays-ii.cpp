bc471a7b94c8f306d3fceee42d71b53a4cc8a65f
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-05-04 15:19:42 +0530

    Time: 4 ms (15.73%), Space: 14 MB (98.93%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int>set;
+        vector<int>temp;
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                if(nums1[i]==nums2[j]){
+                    temp.push_back(nums2[j]);
+                    nums2[j]=-1;
+                    break;
+                }
+            }
+        }
+        return temp;      
+    }
+};
