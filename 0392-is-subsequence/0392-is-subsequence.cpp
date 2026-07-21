447eaed1188d16e05b5f28bcb6949de7be9468b5
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-07-21 15:52:48 +0530

    Time: 0 ms (100%), Space: 8.6 MB (67.91%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int n = s.size();
-        int j = 0;
+        int j=0;
         for(int i=0;i<t.size();i++){
-            if(s[j]==t[i]){
-                j++;
-            }
+            if(s[j]==t[i]) j++;
         }
-        if(j == s.size()) return 1;
+        if(j==n) return 1;
         return 0;
     }
 };
