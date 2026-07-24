99523d3595d2d58917a59ce3918f082d5353b886
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-07-24 21:02:49 +0530

    Time: 0 ms (100%), Space: 30.3 MB (79.94%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> concatWithReverse(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n*2,0);
+        vector<int>ans(n*2,0);
         for(int i=0;i<n;i++){
             ans[i] = nums[i];
             ans[i+n] = nums[n-i-1];
