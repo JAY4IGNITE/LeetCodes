e4e9abf753b0d4dad8078d5b8b48ff444995f952
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-07-25 21:05:30 +0530

    Time: 3 ms (77.39%), Space: 15.6 MB (47.07%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool winnerOfGame(string s) {
-        int acnt=0,bcnt=0,a=0,b=1,c=2;
-        while(c<s.size()){
-            if(s[a]=='A'&&s[b]=='A'&&s[c]=='A') acnt++;
-            if(s[a]=='B'&&s[b]=='B'&&s[c]=='B') bcnt++;
-            a++;
-            b++;
-            c++;
+        int cnt=0,cnt1=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]==s[i+1]&&s[i+1]==s[i+2]){
+                if(s[i]=='A') cnt++;
+                else cnt1++;
+            }
         }
-        if(acnt>bcnt) return 1;
-        return 0;
+        return cnt>cnt1;
     }
 };
