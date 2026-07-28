f8e4bd1f92abd955e83e99b9af2fe26d4151f483
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-07-28 06:17:19 +0530

    Time: 0 ms (100%), Space: 8.3 MB (69.63%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     char repeatedCharacter(string s) {
-        set<char>st;
+        map<char,int> mp;
+        int left = 0;
+        int right = s.size()-1;
         for(int i=0;i<s.size();i++){
-            if(!st.empty()&&st.find(s[i])!=st.end()){
-                return s[i];
-            }
-            else{
-                st.insert(s[i]);
+            for(int j=0;j<i;j++){
+                if(s[i]==s[j]){
+                    return s[i];
+                }
             }
         }
         return ' ';
