a27c8a8724af5d27cc3fab9742aed645ce7155b3
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-07-29 15:50:45 +0530

    Time: 7 ms (21.08%), Space: 18.4 MB (7.83%) - LeetHub

---
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        map<int,int> m;
+        for(auto x:nums){
+            m[x]++;
+        }
+        for(auto x:m){
+            if(x.second%2!=0) return 0;
+        }
+        return 1;
+    }
+};
