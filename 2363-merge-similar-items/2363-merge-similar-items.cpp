26effec86c6ddf7b889ac9b0ec73fbd33456f1f6
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-08-01 10:19:08 +0530

    Time: 9 ms (31.54%), Space: 21.9 MB (10.3%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.cpp
+++ 2363-merge-similar-items/2363-merge-similar-items.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
-    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& item1, vector<vector<int>>& item2) {
-        map<int,int> mp;
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        map<int,int> a;
+        for(auto i:items1) a[i[0]]+=i[1];
+        for(auto i:items2) a[i[0]]+=i[1];
         vector<vector<int>> ans;
-        for(auto item:item1){
-            mp[item[0]] += item[1];
-        }
-        for(auto item:item2){
-            mp[item[0]] += item[1];
-        }
-        for(auto x:mp){
-            ans.push_back({x.first,x.second});
-        }
+        for(auto i:a) ans.push_back({i.first,i.second});
         return ans;
     }
 };
