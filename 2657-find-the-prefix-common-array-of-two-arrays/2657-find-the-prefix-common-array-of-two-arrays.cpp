6290d1059a67b37e41fdee700ef3f7d24c841573
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-08-04 20:33:24 +0530

    Time: 18 ms (18.65%), Space: 85.9 MB (65.46%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -3,10 +3,11 @@ public:
     vector<int> findThePrefixCommonArray(vector<int>& a, vector<int>& b) {
         vector<int>ans;
         int n = a.size();
-        for(int l=0;l<n;l++){
+        for(int st=0;st<n;st++){
             int cnt = 0;
-            for(int i=0;i<=l;i++){
-                if(find(a.begin(),a.begin()+l+1,b[i])!=a.begin()+l+1){
+            for(int i=0;i<=st;i++){
+                auto it = find(a.begin(),a.begin()+st+1,b[i])!=a.begin()+st+1;
+                if(it){
                     cnt++;
                 }
             }
