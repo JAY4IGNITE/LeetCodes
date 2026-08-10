aeee8949e0f92036d1d93f8857869277ed42f0ee
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-08-10 14:02:16 +0530

    Time: 4 ms (33.8%), Space: 18 MB (43.47%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -5,15 +5,15 @@ public:
         for(int i=0;i<nums.size();i++){
             m[nums[i]]++;
         }
-        priority_queue<pair<int,int>>p;
+        priority_queue<pair<int,int>>a;
         for(auto x:m){
-            p.push({x.second,x.first});
+            a.push({x.second,x.first});
         }
-        vector<int>a;
+        vector<int>b;
         for(int i=0;i<k;i++){
-            a.push_back(p.top().second);
-            p.pop();
+            b.push_back(a.top().second);
+            a.pop();
         }
-        return a;
+        return b;
     }
 };
