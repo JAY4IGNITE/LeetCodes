237507f5274068032ab458250108f665431746dc
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-08-13 19:22:44 +0530

    Time: 23 ms (1.63%), Space: 266 MB (5.98%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
+        int n=nums.size();
         k = k%n;
-        deque<int> dq(nums.begin(), nums.end());
-        for (int i = 0; i < k; i++) {
+        deque<int>dq(nums.begin(),nums.end());
+        for(int i=0;i<k;i++){
             int x = dq.back();
             dq.pop_back();
             dq.push_front(x);
         }
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
             nums[i] = dq.front();
             dq.pop_front();
         }
