f80e77d931815869cde3e6c3244b1e949d0ba96d
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-08-16 09:16:01 +0530

    Time: 1175 ms (5.04%), Space: 96 MB (5.1%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.size() > s2.size())
-            return 0;
-        unordered_map<char, int> need, window;
-        for (char c : s1)
-            need[c]++;
-        int k = s1.size();
-        for (int i = 0; i < k; i++)
-            window[s2[i]]++;
-        if (window == need)
-            return 1;
-        for (int right = k; right < s2.size(); right++) {
-            window[s2[right]]++;
-            window[s2[right - k]]--;
-            if (window[s2[right - k]] == 0)
-                window.erase(s2[right - k]);
-            if (window == need)
-                return 1;
+        int  n=s1.size();
+        if(n>s2.size()) return 0;
+        map<char,int> mp;
+        for(auto x:s1){
+            mp[x]++;
+        }
+        for(int i=0;i<=s2.size()-n;i++){
+            map<char,int> mp1;
+            for(int j=i;j<i+n;j++){
+                mp1[s2[j]]++;
+            }
+            if(mp1==mp) return 1;
         }
         return 0;
     }
