6f3ce03171380f780614da5dc28d63def9ecc8d4
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-08-16 17:44:05 +0530

    Time: 4 ms (10.51%), Space: 71.8 MB (53.32%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi = nums[0];
+        int curr = nums[0];
+        for(int i=1;i<nums.size();i++){
+            curr = max(nums[i],nums[i]+curr);
+            maxi = max(maxi,curr);
+        }
+        return maxi;
+    }
+};
