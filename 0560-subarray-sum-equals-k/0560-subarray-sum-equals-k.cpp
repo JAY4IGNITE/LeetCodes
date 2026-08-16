fae45cc65fc26594eb5625be83b7c8d26162f3c4
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-08-16 18:01:49 +0530

    Time: 71 ms (29.27%), Space: 47.7 MB (25.86%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        map<int, int> mp;
-        mp[0] = 1;
-        int sum = 0;
-        int ans = 0;
-        for (auto x : nums) {
-            sum += x;
-            if (mp.count(sum - k)) {
-                ans += mp[sum - k];
+        map<int,int>mp;
+        mp[0]=1;
+        int prefixSum = 0;
+        int cnt=0;
+        for(auto x:nums){
+            prefixSum += x;
+            if(mp.count(prefixSum-k)){
+                cnt += mp[prefixSum - k];
             }
-            mp[sum]++;
+            mp[prefixSum]++;
         }
-        return ans;
+        return cnt;
     }
 };
