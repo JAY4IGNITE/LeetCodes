fc0e12986d0e9046d7e12ff75e9085489518fe48
JAYA SAI KRISHNA <24b11cs514@adityauniversity.in>
2026-08-20 06:25:03 +0530

    Time: 0 ms (100%), Space: 23.9 MB (66.37%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> ans1;
+        vector<int> ans2;
+        ans1.push_back(nums[0]);
+        ans2.push_back(nums[1]);
+        for (int i = 2; i < nums.size(); i++) {
+            if (ans1.back() > ans2.back()) {
+                ans1.push_back(nums[i]);
+            } else {
+                ans2.push_back(nums[i]);
+            }
+        }
+        for (int x : ans2) {
+            ans1.push_back(x);
+        }
+        return ans1;
+    }
+};
